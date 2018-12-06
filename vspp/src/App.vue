9a641292a294505e0742730a85c4e7282f933b27
Kurosh Farsimadan <kurosh_farsimadan@yahoo.com>
2018-12-05 20:25:13 -0500

    Add files via upload

---
+++ vspp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header/>
+    <div class="app-content">
+      <router-view></router-view>
+      <!-- <Home/> -->
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Home from "./components/Home.vue";
+
+export default {
+  name: "app",
+  components: {
+    Footer,
+    Header,
+    Home
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vspp/src/components/Test.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b>
+    Tester tests his testings before he wins 1.500 billoin lottery. Here we could use manual testing for methods, components, or others.
+    It is highly recommended to use Jenkins or automated testing tools or custom built libraries.
+
+    <div v-on:scroll.passive="onScroll">...</div>
+
+
+    When listening for keyboard events, we often need to check for common key codes. Vue also allows adding key modifiers for v-on when listening for key events:
+
+    <!-- only call `vm.submit()` when the `keyCode` is 13 -->
+    <input v-on:keyup.13="submit">
+    Remembering all the keyCodes is a hassle, so Vue provides aliases for the most commonly used keys:
+
+    <!-- same as above -->
+    <input v-on:keyup.enter="submit">
+
+    <!-- also works for shorthand -->
+    <input @keyup.enter="submit">
+  </b>
+</div>
+</template>
+
+<script>
+import { bus } from "../main";
+
+export default {
+  name: "Test",
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
+
+
